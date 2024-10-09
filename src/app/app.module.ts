52c5df2bb81f4b12656a626a3a6fd196fb979891
Silvio Aquino <silvioaquino@Silvios-MacBook-Pro.local>
2024-10-09 15:15:11 -0300

    feat: iniciei a aula 4E, porem a imagem nao aparace e o botao nao abre o alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SeguntoComponenteComponent } from './segunto-componente/segunto-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    SeguntoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
