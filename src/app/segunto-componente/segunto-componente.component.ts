23a8592f3972f4e72dcead526d28eb84872f47e8
silvioaquino <silvios.aquino@gmail.com>
2024-10-09 20:40:17 -0300

    feat: correção da exibição da imagem e do botão

--- src/app/segunto-componente/segunto-componente.component.ts
+++ src/app/segunto-componente/segunto-componente.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-segunto-componente',
   templateUrl: './segunto-componente.component.html',
-  styleUrl: './segunto-componente.component.css'
+  styleUrl: './segunto-componente.component.css',
+  
 })
 export class SeguntoComponenteComponent {
   nome = "Silvio"
   dataNascimento = "1983-01-02"
-  urlImagem = "/Estudo-Angular/Fundamentos-Angular/src/assets/DELIVERY-CORONA.jpg"
+  urlImagem = "/assets/imagem.png"
 
   mostrarDataNascimento() {
     alert(`A data de nascimento de ${this.nome} é: ${this.dataNascimento}`)
