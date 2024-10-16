4db21a280b0596f12d1199efa49cc50a4c1e3a44
SilvioAquinoDev <silvio.aquinodev@gmail.com>
2024-10-15 21:16:03 -0300

    Revert "Feature/estudos no trabalho"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SeguntoComponenteComponent } from './segunto-componente/segunto-componente.component';
 import { MultiplicaPorPipe } from './multiplica-por.pipe';
 import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
-import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
-import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
-import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
-import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
-import { Servico01Component } from './servico01/servico01.component';
-import { Servico02Component } from './servico02/servico02.component';
-import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
 
 @NgModule({
   declarations: [
@@ -21,19 +14,11 @@ import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
     PrimeiroComponenteComponent,
     SeguntoComponenteComponent,
     MultiplicaPorPipe,
-    PipeCustomizadoComponent,
-    RenderizandoListasComponent,
-    ComponentePersonalizadoComponent,
-    ComponenteFilhoComponent,
-    ComponentePaiComponent,
-    Servico01Component,
-    Servico02Component,
-    CicloDeVidaComponent
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [
     provideClientHydration()
